aec3a459ea09f2efb939c21f3b35395e91b811c9
Oliver Scharkowski <oscharko@MacBook-Pro-von-Oliver.local>
2026-09-06 20:17:21 +0200

    fix: make average ignore non-finite values

--- ci/numerical-stability.ci.js
+++ ci/numerical-stability.ci.js
@@ -7,3 +7,7 @@ const { average } = require("../index.js");
 test("average remains finite when the mathematical mean is finite", () => {
   assert.equal(average([Number.MAX_VALUE, Number.MAX_VALUE]), Number.MAX_VALUE);
 });
+
+test("average remains finite while ignoring non-finite values", () => {
+  assert.equal(average([Number.MAX_VALUE, Infinity, Number.MAX_VALUE, NaN]), Number.MAX_VALUE);
+});
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const finiteValues = require("./lib/finite-values.js");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,13 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const numbers = finiteValues(values);
+
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  return numbers.reduce((mean, value, index) => mean + (value - mean) / (index + 1), 0);
 }
 
 module.exports = { sum, average };

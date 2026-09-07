1805b4995ca35242b5af56e5700f024d58b3a96f
oscharko <o.scharkowski@oscharko.de>
2026-09-07 19:39:11 +0200

    fix: handle empty and non-finite values in average

--- ci/numerical-stability.ci.js
+++ ci/numerical-stability.ci.js
@@ -5,5 +5,5 @@ const assert = require("node:assert/strict");
 const { average } = require("../index.js");
 
 test("average remains finite when the mathematical mean is finite", () => {
-  assert.equal(average([Number.MAX_VALUE, Number.MAX_VALUE]), Number.MAX_VALUE);
+  assert.strictEqual(average([Number.MAX_VALUE, Number.MAX_VALUE]), Number.MAX_VALUE);
 });
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const selectFiniteNumbers = require("./lib/selectFiniteNumbers");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,13 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const finiteValues = selectFiniteNumbers(values);
+
+  if (finiteValues.length === 0) {
+    return 0;
+  }
+
+  return finiteValues.reduce((mean, value, index) => mean + (value - mean) / (index + 1), 0);
 }
 
 module.exports = { sum, average };

f52a06f097f1aa53b4873e50c1456d3a2769fb52
oscharko <o.scharkowski@oscharko.de>
2026-09-08 10:17:44 +0200

    fix: handle empty and non-finite average inputs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const finiteValues = require("./lib/finite-values.js");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,19 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const numbers = finiteValues(values);
+
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  let mean = 0;
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    mean += (numbers[index] - mean) / (index + 1);
+  }
+
+  return mean;
 }
 
 module.exports = { sum, average };

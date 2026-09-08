f8a57c780d270630e57d59428d54ba40f7bca0f7
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2026-09-08 18:34:16 +0200

    fix: make average ignore non-finite values safely (#36)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const selectFiniteNumbers = require("./lib/selectFiniteNumbers.js");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,25 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const finiteValues = selectFiniteNumbers(values);
+
+  if (finiteValues.length === 0) {
+    return 0;
+  }
+
+  let mean = finiteValues[0];
+
+  for (let index = 1; index < finiteValues.length; index += 1) {
+    const value = finiteValues[index];
+
+    if ((mean < 0 && value > 0) || (mean > 0 && value < 0)) {
+      mean = mean + value / (index + 1) - mean / (index + 1);
+    } else {
+      mean = mean + (value - mean) / (index + 1);
+    }
+  }
+
+  return mean;
 }
 
 module.exports = { sum, average };

40f45cc111533f9568fc0d0765b77920295e0ffd
oscharko <o.scharkowski@oscharko.de>
2026-09-08 16:25:17 +0200

    fix: handle empty and non-finite values in average

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const selectFiniteNumbers = require("./lib/selectFiniteNumbers");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,23 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const finiteValues = selectFiniteNumbers(values);
+
+  if (finiteValues.length === 0) {
+    return 0;
+  }
+
+  let mean = 0;
+
+  for (let index = 0; index < finiteValues.length; index += 1) {
+    const count = index + 1;
+    const value = finiteValues[index];
+
+    mean = mean / count * index + value / count;
+  }
+
+  return Object.is(mean, -0) ? 0 : mean;
+
 }
 
 module.exports = { sum, average };

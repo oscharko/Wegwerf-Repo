80e33c73ffc2197926e06b9a41bb2fe972fbf2e6
oscharko <o.scharkowski@oscharko.de>
2026-09-08 10:30:37 +0200

    fix: handle empty and non-finite averages safely

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const finiteNumbers = require("./lib/finite-numbers.js");
+
 /**
  * Sum a list of numbers.
  * @param {number[]} values
@@ -15,7 +17,16 @@ function sum(values) {
  * @returns {number}
  */
 function average(values) {
-  return sum(values) / values.length;
+  const numbers = finiteNumbers(values);
+
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  const meanOffset = numbers[0];
+  const centeredTotal = numbers.reduce((total, value) => total + (value - meanOffset), 0);
+
+  return meanOffset + centeredTotal / numbers.length;
 }
 
 module.exports = { sum, average };
